17b351922f7c4ec71d9c4c94d88092aa63d138fc
delphin <delphinrukundo10@gmail.com>
2022-12-03 00:26:02 +0100

    add css modules

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,10 +2,11 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Head from 'next/head';
 import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -24,6 +25,6 @@ export default function FirstPost() {
         width={144} // Desired size with correct aspect ratio
         alt="nextjs"
       />
-    </>
+    </Layout>
   );
 }
