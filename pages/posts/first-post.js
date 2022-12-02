f46fc66d5d10a4096d8aae9d7fd9abfb21034b0b
delphin <delphinrukundo10@gmail.com>
2022-12-02 23:57:35 +0100

    add the first-post

---
+++ pages/posts/first-post.js
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import Script from 'next/script';
+
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://kit.fontawesome.com/2c36e9b7b1.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log('Font Awesome loaded')}
+      />
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="nextjs"
+      />
+    </>
+  );
+}
